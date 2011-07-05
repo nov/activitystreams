471a7d6c283ee4f1b7b91005cf401e96101f14a2
nov matake <nov@matake.jp>
2011-07-05 17:57:46 +0900

    now coverage 100%

--- spec/activitystreams/collection_spec.rb
+++ spec/activitystreams/collection_spec.rb
@@ -1,5 +1,44 @@
 require 'spec_helper'
 
 describe ActivityStreams::Collection do
-  
+  let(:collection) { ActivityStreams::Collection.new(:items => items) }
+
+  context 'when array of Activity given' do
+    let :items do
+      [
+        ActivityStreams::Activity.new(
+          :actor => ActivityStreams::Object.new(:display_name => 'Nov'),
+          :published => Time.now
+        )
+      ]
+    end
+
+    it do
+      expect { collection }.should raise_error(
+        ActivityStreams::InvalidAttribute,
+        'items should be an array of ActivityStreams::Object'
+      )
+    end
+  end
+
+  context 'when array of Object given' do
+    let :items do
+      [
+        ActivityStreams::Object.new
+      ]
+    end
+
+    it do
+      expect { collection }.should_not raise_error
+    end
+  end
+
+  context 'when missing both items and url' do
+    it do
+      expect { ActivityStreams::Collection.new }.should raise_error(
+        AttrRequired::AttrMissing,
+        'Either "items" or "url" is required'
+      )
+    end
+  end
 end
--- spec/activitystreams/stream_spec.rb
+++ spec/activitystreams/stream_spec.rb
@@ -1,5 +1,35 @@
 require 'spec_helper'
 
 describe ActivityStreams::Stream do
-  
+  let(:stream) { ActivityStreams::Stream.new(:items => items) }
+
+  context 'when array of Activity given' do
+    let :items do
+      [
+        ActivityStreams::Activity.new(
+          :actor => ActivityStreams::Object.new(:display_name => 'Nov'),
+          :published => Time.now
+        )
+      ]
+    end
+
+    it do
+      expect { stream }.should_not raise_error
+    end
+  end
+
+  context 'when array of Object given' do
+    let :items do
+      [
+        ActivityStreams::Object.new
+      ]
+    end
+
+    it do
+      expect { stream }.should raise_error(
+        ActivityStreams::InvalidAttribute,
+        'items should be an array of ActivityStreams::Activity'
+      )
+    end
+  end
 end
